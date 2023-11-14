a76bcbbf758a517aaea17f9134d500c0adfa5497
Muhammat Lio Pratama <muhammatliopratama@gmail.com>
2023-11-14 23:50:39 +0700

    implemented route middlewares

--- server/routers/admin.js
+++ server/routers/admin.js
@@ -1,17 +1,18 @@
 const express = require("express");
 const AdminController = require("../controllers/adminController");
+const { mustAdmin } = require("../middlewares/authentication");
 
 const router = express.Router();
 
-router.post("/login", AdminController.login);
-router.get("/games", AdminController.getGames);
-router.post("/games", AdminController.createGame);
-router.put("/games/:id", AdminController.updateGame);
-router.delete("/games/:id", AdminController.deleteGame);
-router.get("/games/:id", AdminController.getSession);
-router.get("/games/:id/open", AdminController.openSession);
-router.get("/games/:gameSessionId/start", AdminController.startSession);
-router.get("/games/:gameSessionId/end", AdminController.endSession);
-router.get("/games/:gameSessionId/result", AdminController.getResult);
+router.post("/login", [mustAdmin], AdminController.login);
+router.get("/games", [mustAdmin], AdminController.getGames);
+router.post("/games", [mustAdmin], AdminController.createGame);
+router.put("/games/:id", [mustAdmin], AdminController.updateGame);
+router.delete("/games/:id", [mustAdmin], AdminController.deleteGame);
+router.get("/games/:id", [mustAdmin], AdminController.getSession);
+router.get("/games/:id/open", [mustAdmin], AdminController.openSession);
+router.get("/games/:gameSessionId/start", [mustAdmin], AdminController.startSession);
+router.get("/games/:gameSessionId/end", [mustAdmin], AdminController.endSession);
+router.get("/games/:gameSessionId/result", [mustAdmin], AdminController.getResult);
 
 module.exports = router;
--- server/routers/player.js
+++ server/routers/player.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const PlayerController = require("../controllers/playerController");
+const { mustAuthenticated } = require("../middlewares/authentication");
 const router = express.Router();
 
 router.post("/login", PlayerController.login);
-router.post("/signup", PlayerController.signup);
-router.get("/games/:gameId", PlayerController.getSessionById);
-router.get("/games/:gameId/:questionID", PlayerController.getQuestion);
-router.post("/games/:gameId/:questionID", PlayerController.answerQuestion);
+router.post("/signup", [mustAuthenticated], PlayerController.signup);
+router.get("/games/:gameId", [mustAuthenticated], PlayerController.getSessionById);
+router.get("/games/:gameId/:questionID", [mustAuthenticated], PlayerController.getQuestion);
+router.post("/games/:gameId/:questionID", [mustAuthenticated], PlayerController.answerQuestion);
 
 module.exports = router;
