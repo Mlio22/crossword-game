2c856f80464f9e40e92de97ac720f5b1de8fe9e2
Muhammat Lio Pratama <muhammatliopratama@gmail.com>
2023-11-14 19:59:11 +0700

    added auth middleware

---
+++ server/middlewares/authentication.js
@@ -0,0 +1,59 @@
+const { verifyToken } = require("../helpers/jwt");
+
+function mustAuthenticated(req, res, next) {
+  try {
+    const authorization = req.header.authorization;
+
+    if (!authorization) {
+      throw { name: "unauthorized", message: "please login first" };
+    }
+
+    const [type, token] = authorization.split(" ");
+
+    if (!type || type !== "Bearer") {
+      throw { name: "unauthorized", message: "invalid token" };
+    }
+
+    if (!token) {
+      throw { name: "unauthorized", message: "invalid token" };
+    }
+
+    const { id } = verifyToken(token);
+
+    req.user = { id };
+    next();
+  } catch (error) {
+    next(error);
+  }
+}
+
+function mustAdmin(req, res, next) {
+  try {
+    const authorization = req.header.authorization;
+
+    if (!authorization) {
+      throw { name: "unauthorized", message: "please login first" };
+    }
+
+    const [type, token] = authorization.split(" ");
+
+    if (!type || type !== "Bearer") {
+      throw { name: "unauthorized", message: "invalid token" };
+    }
+
+    if (!token) {
+      throw { name: "unauthorized", message: "invalid token" };
+    }
+
+    const { id } = verifyToken(token);
+
+    if (!id !== "admin") throw { name: "unauthorized", message: "invalid token" };
+
+    req.user = { id };
+    next();
+  } catch (error) {
+    next(error);
+  }
+}
+
+module.exports = { mustAuthenticated, mustAdmin };
