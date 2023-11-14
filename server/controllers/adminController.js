5bb98ab78412b48ed726a867f2f028644fc34ed2
Muhammat Lio Pratama <muhammatliopratama@gmail.com>
2023-11-14 18:12:40 +0700

    added POST /admin/login

--- server/controllers/adminController.js
+++ server/controllers/adminController.js
@@ -1,78 +1,104 @@
+const { hashPass, comparePass } = require("../helpers/bcrypt");
+const { createToken } = require("../helpers/jwt");
+const { Admin } = require("../models");
+
 module.exports = class AdminController {
   static async login(req, res, next) {
     try {
-    } catch (error) {
-      next(error);
-    }
-  }
+      req.body = req.body || {};
 
-  static async logout(req, res, next) {
-    try {
+      const { email, password } = req.body;
+
+      if (!email || !password) {
+        throw { name: "badRequest", message: "Please fill the login form" };
+      }
+
+      const selectedAdmin = await Admin.findOne({
+        where: {
+          email,
+        },
+      });
+
+      if (!selectedAdmin) {
+        throw { name: "unauthorized", message: "Wrong email / password" };
+      }
+
+      if (!comparePass(password, selectedAdmin.password)) {
+        throw { name: "unauthorized", message: "Wrong email / password" };
+      }
+
+      const payload = {
+        id: "admin",
+      };
+
+      const access_token = createToken(payload);
+
+      return res.status(200).json({ access_token });
     } catch (error) {
-      next(error);
+      return next(error);
     }
   }
 
   static async getGames(req, res, next) {
     try {
     } catch (error) {
-      next(error);
+      return next(error);
     }
   }
 
   static async createGame(req, res, next) {
     try {
     } catch (error) {
-      next(error);
+      return next(error);
     }
   }
 
   static async updateGame(req, res, next) {
     try {
     } catch (error) {
-      next(error);
+      return next(error);
     }
   }
 
   static async deleteGame(req, res, next) {
     try {
     } catch (error) {
-      next(error);
+      return next(error);
     }
   }
 
   static async getSession(req, res, next) {
     try {
     } catch (error) {
-      next(error);
+      return next(error);
     }
   }
 
   static async openSession(req, res, next) {
     try {
     } catch (error) {
-      next(error);
+      return next(error);
     }
   }
 
   static async startSession(req, res, next) {
     try {
     } catch (error) {
-      next(error);
+      return next(error);
     }
   }
 
   static async endSession(req, res, next) {
     try {
     } catch (error) {
-      next(error);
+      return next(error);
     }
   }
 
   static async getResult(req, res, next) {
     try {
     } catch (error) {
-      next(error);
+      return next(error);
     }
   }
 };
