917bf96c23086461762142e2f49580a27a3e477c
Muhammat Lio Pratama <muhammatliopratama@gmail.com>
2023-11-15 16:53:08 +0700

    layout: added router provider

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -61,4 +61,8 @@ const router = createBrowserRouter([
   },
 ]);
 
-ReactDOM.createRoot(document.getElementById("root")).render(<React.StrictMode></React.StrictMode>);
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
