015f5aefab050410b039d6f6c65e99f8fb462a03
Muhammat Lio Pratama <muhammatliopratama@gmail.com>
2023-11-17 09:33:36 +0700

    deploy #2

--- server/constants.js
+++ server/constants.js
@@ -1,5 +1,5 @@
 // const CLIENT = "http://localhost:5173";
 const CLIENT = 'https://crossword-game-a5a75.web.app'
-// const CLIENT = 'https://8225-180-252-160-34.ngrok-free.app'
+// const CLIENT = 'https://4a43-139-228-111-126.ngrok-free.app/'
 
 module.exports = CLIENT;
